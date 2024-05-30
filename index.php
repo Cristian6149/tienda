3e8044b89553285cf8959f2e2271644b58714637
Tu Nombre <tucorreo@ejemplo.com>
2024-05-30 17:06:29 -0500

    primera

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Página PHP Simple</title>
+</head>
+<body>
+    <h1>Bienvenido a mi página PHP</h1>
+    
+    <?php
+        // Código PHP aquí
+        echo "¡Hola, mundo!";
+    ?>
+    
+    <p>Esta es una página web básica construida con PHP.</p>
+    
+    <?php
+        // Mostrar la fecha actual
+        echo "Hoy es " . date("d/m/Y");
+    ?>
+</body>
+</html>
